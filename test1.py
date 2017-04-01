c81d661d2f72d588a8d89225aa4a5fac4d778578
MonicaXie <xiemeng3658@live.cn>
2017-04-01 17:03:33 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ for i in range(1, 5):
     for j in range(1, 5):
         for k in range(1, 5):
             if (i != j) and (j != k) and (i != k):
-                print(i, j, k)`
+                print(i, j, k)
 
 # 输出结果
 
