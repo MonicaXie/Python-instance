9d45befea32109abcb33c4b7b8149f0ef2d03eb6
MonicaXie <xiemeng3658@live.cn>
2017-04-03 22:29:05 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -2,4 +2,12 @@
 
 import math
 for i in range(10000):
-    
+    x = int(math.sqrt(i + 100))
+    y = int(math.sqrt(i + 268))
+    if (x * x == i + 100) and (y * y == i + 268):
+        print(i)
+
+# 输出结果：
+21
+261
+1581
