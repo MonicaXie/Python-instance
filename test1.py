17773658c20d0a6c14bc8383acb86709b4d4282d
MonicaXie <xiemeng3658@live.cn>
2017-04-03 20:01:31 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -13,7 +13,7 @@ print(nums)
 
 # 方法二
 
-L = list(x * 100 + y * 10 + z for x in range(1, 6) for y in range(1, 6) for z in range(1, 6) if (x != y) and (y != z) and (x != z))
+L = [x * 100 + y * 10 + z for x in range(1, 6) for y in range(1, 6) for z in range(1, 6) if (x != y) and (y != z) and (x != z)]
 print(len(L))
 print(L)
         
