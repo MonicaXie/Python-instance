d3b270a228a583bce3cb52e5d8696884c35522f9
MonicaXie <xiemeng3658@live.cn>
2017-04-07 18:26:41 +0800

    Update test8-乘法表.py

--- test8-乘法表.py
+++ test8-乘法表.py
@@ -23,9 +23,9 @@ for i in range(1, 10):
 
 for i in range(1, 10):
     for k in range(1, i):
-        print(end = '            ') # 字符串空格为14个，每个算式的位置是14个字节
+        print(end = '              ') # 字符串空格为14个，每个算式的位置是14个字节
     for j in range(i, 10):
-        print('%d * %d = %2d' % (i, j, i * j), end = '  ')
+        print('%d * %d = %2d' % (i, j, i * j), end = '    ')
     print()
     
 # 输出结果
