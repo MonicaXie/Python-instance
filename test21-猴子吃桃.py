4b851728387782363ed925387eb9b599018ab8b7
MonicaXie <xiemeng3658@live.cn>
2017-04-12 16:44:48 +0800

    Update test21-猴子吃桃.py

--- test21-猴子吃桃.py
+++ test21-猴子吃桃.py
@@ -4,10 +4,23 @@
 # 到第十天早上想再吃时，见只剩下一个桃子了。
 # 求第一天共摘了多少。
 
+# 方法一：
+
 n = 1
 for i in range(9, 0, -1):
     n = (n + 1) * 2
 print('第一天共摘了%d个桃子' % n)
+
+# 方法二：
+
+def num(day = 1):
+    if not isinstance(day, int) or day > 10 or day <= 0:
+        return -1
+    elif day == 10:
+        return 1
+    else:
+        return (num(day + 1) + 1) * 2
+print('第一天共摘了%d个桃子' % num())
 
 # 输出结果
 
